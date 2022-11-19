5147885ef32d5368d4876528346ff88ebe6725fe
EvgenyM <MelnikovEI@ya.ru>
2022-11-19 18:00:52 +0300

    add verbose names

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,14 +3,14 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     """Pokemon model"""
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    description = models.TextField(blank=True)
-    photo = models.ImageField(null=True, upload_to='pokemon_pictures')
+    title = models.CharField('название', max_length=200)
+    title_en = models.CharField('название на английском языке', max_length=200, blank=True)
+    title_jp = models.CharField('название на японском языке', max_length=200, blank=True)
+    description = models.TextField('описание', blank=True)
+    photo = models.ImageField('изображение', null=True, upload_to='pokemon_pictures')
 
     previous_evolution = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,
-                                           related_name='next_evolution')
+                                           related_name='next_evolution', verbose_name='Из кого эволюционировал')
 
     def __str__(self):
         return self.title
@@ -18,17 +18,17 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     """Pokemon coordinates"""
-    lat = models.FloatField()
-    lon = models.FloatField()
+    lat = models.FloatField('широта')
+    lon = models.FloatField('долгота')
     pokemon = models.ForeignKey(
         Pokemon,
         verbose_name='Покемон',
         on_delete=models.CASCADE
     )
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strength = models.IntegerField()
-    defence = models.IntegerField()
-    stamina = models.IntegerField()
+    appeared_at = models.DateTimeField('момент появления на карте')
+    disappeared_at = models.DateTimeField('момент исчезновления с карты')
+    level = models.IntegerField('уровень')
+    health = models.IntegerField('здоровье')
+    strength = models.IntegerField('сила')
+    defence = models.IntegerField('защита')
+    stamina = models.IntegerField('выносливость')
