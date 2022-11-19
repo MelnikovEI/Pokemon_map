2a71178a4ba83b06d714f8ed59d029ce6952bd49
EvgenyM <MelnikovEI@ya.ru>
2022-11-19 12:17:05 +0300

    add en and jp titles

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,8 +4,10 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     """Pokemon model"""
     title = models.CharField(max_length=200)
-    photo = models.ImageField(null=True, upload_to='pokemon_pictures')
+    title_en = models.CharField(max_length=200, blank=True)
+    title_jp = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True)
+    photo = models.ImageField(null=True, upload_to='pokemon_pictures')
 
     def __str__(self):
         return self.title
